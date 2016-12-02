c5cf1b5316363c47368709bf1167e55ea67f339e
Vince Roti <vince.roti@gmail.com>
2016-12-02 15:17:27 -0600

    reverted database settings

--- config/database.rb
+++ config/database.rb
@@ -36,8 +36,8 @@ ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
   :port     => db.port,
-  :username => 'vince',
-  :password => 'postgres',
+  :username => db.user,
+  :password => db.password,
   :database => DB_NAME,
   :encoding => 'utf8'
 )
