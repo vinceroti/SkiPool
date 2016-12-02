9e8044be8be80a4cae1bad2f0ac6f608088fd2b1
Vince Roti <vince.roti@gmail.com>
2016-12-02 11:46:35 -0600

    working embedded webcam

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,11 +1,25 @@
 get '/mountains/:mountain_id' do
+
   @mountain = MountainStation.find(params[:mountain_id])
   @temp_f = @mountain.current_observation["temp_f"]
   @temp_c = @mountain.current_observation["temp_c"]
-  p @xml_doc = Nokogiri::XML(open("http://www.myweather2.com/developer/weather.ashx?uac=G2etkq56ct&uref=714c6b47-8cb0-4925-a4c1-88118d68d76d"))
+  @xml_doc = Nokogiri::XML(open("http://www.myweather2.com/developer/weather.ashx?uac=G2etkq56ct&uref=714c6b47-8cb0-4925-a4c1-88118d68d76d"))
   if !@temp_f || !@temp_c
     "Sorry, server issues"
   else
     erb :'mountains/show'
   end
+end
+
+get '/mountains/:mountain_id/webcam' do
+  @mountain = MountainStation.find(params[:mountain_id])
+  response = JSON.parse(open("https://webcamstravel.p.mashape.com/webcams/list/nearby=39.5792,-105.9347,5?show=webcams:image",
+    "X-Mashape-Key" => "z40MPygxW3mshJmuIzjgWTkV6GiXp13WFyUjsnAU30HUbmBZju").read)
+   @webcam_image = response["result"]["webcams"][2]["image"]["current"]["preview"]
+
+  timelaspe_response = JSON.parse(open("https://webcamstravel.p.mashape.com/webcams/list/nearby=39.5792,-105.9347,5?show=webcams:timelapse",
+  "X-Mashape-Key" => "z40MPygxW3mshJmuIzjgWTkV6GiXp13WFyUjsnAU30HUbmBZju").read)
+   p @timelaspe_link = timelaspe_response["result"]["webcams"][2]["timelapse"]["day"]["link"]
+
+   erb :'webcams/show'
 end
--- app/models/mountain_station.rb
+++ app/models/mountain_station.rb
@@ -2,7 +2,6 @@ class MountainStation < ActiveRecord::Base
   validates :pws_id, :name, presence: true
 
   def current_observation
-    p W_API.conditions_for("pws: #{self.pws_id}")
     W_API.conditions_for("pws: #{self.pws_id}")["current_observation"]
   end
 
